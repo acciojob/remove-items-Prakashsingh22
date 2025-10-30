2a0e179a4ba673cd05649004ffb82e1857ab19ae
Prakashsingh22 <109896446+Prakashsingh22@users.noreply.github.com>
2025-10-31 00:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// Get references to the dropdown and button
+const select = document.getElementById("colorSelect");
+const button = document.querySelector("input[type='button']");
+
+// Add click event
+button.addEventListener("click", function() {
+  // Get selected option index
+  const selectedIndex = select.selectedIndex;
+
+  // Check if any option is selected
+  if (selectedIndex !== -1) {
+    // Remove the selected option
+    select.remove(selectedIndex);
+  } else {
+    alert("Please select an option to remove.");
+  }
+});
